8113ffddc81e189597dd34c4d48bc1e7867c1e93
Alexander <CaH4ec23@yandex.ru>
2024-07-04 01:59:07 +0300

    second commit

--- precode_test.go
+++ precode_test.go
@@ -3,34 +3,53 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestMainHandlerWhenOk(t *testing.T) {
+const WRONH_CITY_ERROR = "wrong city value"
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=9&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	statusCode := responseRecorder.Code
+	require.NotEqual(t, 0, statusCode)
+	require.Equal(t, http.StatusOK, statusCode)
+
+	responseBody := strings.Split(responseRecorder.Body.String(), ",")
+	assert.Equal(t, totalCount, len(responseBody))
+}
+
+func TestMainHandlerWhenOK(t *testing.T) {
 	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	if status := responseRecorder.Code; status != http.StatusOK {
-		t.Errorf("expected status code: %d, got %d", http.StatusOK, status)
-	}
+	statusCode := responseRecorder.Code
+
+	require.Equal(t, http.StatusOK, statusCode)
+	require.NotEmpty(t, responseRecorder.Body.String())
 }
 
 func TestMainHandlerWhenMissingCount(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?city=moscow", nil)
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=rostov", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	if status := responseRecorder.Code; status != http.StatusBadRequest {
-		t.Errorf("expected status code: %d, got %d", http.StatusBadRequest, status)
-	}
+	statusCode := responseRecorder.Code
 
-	expected := `count missing`
-	if responseRecorder.Body.String() != expected {
-		t.Errorf("expected body: %s, got %s", expected, responseRecorder.Body.String())
-	}
+	require.Equal(t, http.StatusBadRequest, statusCode)
+	require.Equal(t, WRONH_CITY_ERROR, responseRecorder.Body.String())
 }
